051f599bff546c679671ef22f1a9bd4d9d894d31
rebettendorf <ren.bettendorf@karlstorz.com>
2016-02-02 16:07:37 -0800

    Cleaned up BballRefLogic

--- BballRefLogic.py
+++ BballRefLogic.py
@@ -14,17 +14,13 @@ def scrapeHeader(url):
 	return header
 	
 def scrapeURL(url):
-	#url = input("What basketball-reference player would you like to scrape their data?\n")
-	#print("Connecting to website...")
 
 	response = requests.get(url)
-	#print("Succesful connection. Attempting to parse html")
 
 	html = response.content
 	soup = BeautifulSoup(html, "html.parser")
 	table = soup.find('table', attrs={'id': 'per_game'})
 
-	#print("Succesfully parsed html. Now parsing table")
 	header = [header.text for header in table.findAll('th')]
 	tableData = {}
 	for key in table.findAll('th'):
@@ -41,15 +37,7 @@ def scrapeURL(url):
 	json_string = json.dumps(tableData, indent=4)
 	playerFile = soup.find('div', attrs={'id': 'info_box'}).h1.text.replace(" ", "") + "Stats.json"
 	
-	#print("Successfully parsed table.")
-
 	with open(playerFile, "w") as outfile:
 		outfile.write(json_string)
-		
-	#print("Successfully printed data to " + playerFile)
 	
 	return playerFile
-
-
-	
-scrapeURL("http://www.basketball-reference.com/players/c/curryst01.html")
